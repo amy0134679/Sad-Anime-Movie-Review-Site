f8f2101f3b2d978a013d310eae9dc6ee52478902
Amy <amy0134679@gmail.com>
2023-05-10 12:54:56 -0400

    Styled css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,10 @@
       </h1>
     </header>
 
+    <div>
+      <img id="background" :src= "`https://images8.alphacoders.com/749/749455.png`" alt="">
+    </div>
+
     <label for="moviePicker">Select a Movie!:</label>
     <select v-model="moviePicker">
       <option value="378064">A Silent Voice</option>
@@ -22,33 +26,21 @@
     </select>
     <button @click="getInfo">GET</button>
 
-    <div v-if="movieInfo" class="info-container">
-      <h1>{{ movieInfo.title }}</h1> 
+    <div v-if="movieInfo" class="grid-container basicInfo">
+      <h2>{{ movieInfo.title }}</h2> 
       <img :src= "`https://image.tmdb.org/t/p/w500${movieInfo.poster_path}`" alt="">
       <iframe :src= "`https://www.youtube.com/embed/${movieInfo.videos.results.filter((trailer) => trailer.type === 'Trailer').at(0).key}`" frameborder="0"></iframe>
-<!-- 
-      <div class="info">
-        
-        <p>{{ movieInfo.title }}</p> 
-        <h1>{{ movieInfo.title }}</h1> 
-        <h1>{{ movieInfo.title }}</h1> 
-        <p>Release Date: {{movieInfo.release_date}} | Original Language: {{movie.original_language}} | Runtime: {{movieInfo.runtime}} | Movie status: {{movieInfo.movie.status}}</p>
-        <p> {{movieInfo.original_title}} | Rating: {{movieInfo.vote_average}}</p>
-        <h2>Overview:</h2>
-        <p> {{ movieInfo.overview }}</p>
-        <p>"{{movieInfo.tagline}}"</p>
-      </div> -->
     </div>
-    <!-- <div id="movieInfo">
-        <h3 id="movieYear"></h3>
-        <h3 id="ogTitle"></h3>
-        <p id="ogCountry"></p>
-        <p id="ogLang"></p>
-        <p id="runtime"></p>
-        <p id="status"></p>
-        <p id="rating"></p>
-        <p id="movieOverview"></p>
-      </div> -->
+
+    <div v-if="movieInfo" class="grid-container detailInfo"> 
+      <h3>{{ movieInfo.origin_country }}</h3> 
+      <h3>{{ movieInfo.original_title }}</h3> 
+      <p>{{ movieInfo.original_language }}</p> 
+      <p>{{ movieInfo.runtime }}</p> 
+      <p>{{ movieInfo.status }}</p> 
+      <p>{{ movieInfo.vote_average }}</p> 
+
+    </div>
     </div>
 
 </template>
@@ -76,6 +68,7 @@ export default {
       );
       movieInfo.value = response.data;
       console.log(movieInfo.value);
+      console.log(movieInfo.status)
     };
 
     return { moviePicker, movieInfo, getInfo };
@@ -84,16 +77,17 @@ export default {
 </script>
 
 <style scoped>
+
 .body {
   margin: 20px;
-
   font-family: Verdana, Geneva, Tahoma, sans-serif;
 }
-body {
-  margin: 20px;
-  background-image: url("https://images8.alphacoders.com/749/749455.png");
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-}
+#background {
+    position: absolute;
+    z-index: -1;
+    width:100%;
+    height:100%;
+  }
 
 header h1 {
   border-radius: 15px;
@@ -120,7 +114,7 @@ label {
   font-weight: bold;
 }
 
-.select {
+.moviePicker {
   display: flex;
   align-items: center;
   justify-content: center;
@@ -151,6 +145,8 @@ button:hover {
   display: grid;
   grid-gap: 20px;
   margin: 20px;
+  background-color: rgba(252, 210, 235, 0.25);
+
 }
 
 .basicInfo {
@@ -179,7 +175,7 @@ button:hover {
   border-radius: 5px;
 }
 
-#movieInfo {
+.basicInfo {
   color: white;
   justify-content: center;
   padding: 20px;
@@ -188,15 +184,9 @@ button:hover {
   background-color: rgba(252, 210, 235, 0.25);
 }
 
-#movieYear {
-  margin-top: 0;
-  font-size: 1.5rem;
-}
-
-#ogTitle {
-  margin-top: 0;
-  font-size: 1.5rem;
-  font-weight: bold;
+.movieDetails{
+ margin-top: 0;
+font-size: 1.5rem; 
 }
 
 #moviePicker {
