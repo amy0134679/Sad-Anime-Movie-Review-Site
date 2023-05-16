f3d4b8f12aab8588c5c297d1dcf48682366c9b24
Amy <amy0134679@gmail.com>
2023-05-16 00:27:13 -0400

    Fixed id names

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,11 +8,11 @@
     </header>
 
     <div>
-      <img id="background" :src= "`https://images8.alphacoders.com/749/749455.png`" alt="">
+      <img id="background" :src="`https://images8.alphacoders.com/749/749455.png`" alt="">
     </div>
 
-    <label for="moviePicker">Select a Movie!:</label>
-    <select v-model="movie-picker">
+    <label for="movie-picker">Select a Movie!:</label>
+    <select v-model="moviePicker">
       <option value="378064">A Silent Voice</option>
       <option value="12477">Grave of Fireflies</option>
       <option value="530079">Ride Your Wave</option>
@@ -26,13 +26,13 @@
     </select>
     <button @click="getInfo">GET</button>
 
-    <div v-if="movieInfo" class="grid-container basicInfo">
+    <div v-if="movieInfo" class="grid-container basic-info">
       <h2>{{ movieInfo.title }}</h2> 
-      <img :src= "`https://image.tmdb.org/t/p/w500${movie-info.poster_path}`" alt="">
-      <iframe :src= "`https://www.youtube.com/embed/${movie-info.videos.results.filter((trailer) => trailer.type === 'Trailer').at(0).key}`" frameborder="0"></iframe>
+      <img :src="`https://image.tmdb.org/t/p/w500${movieInfo.poster_path}`" alt="">
+      <iframe :src="`https://www.youtube.com/embed/${movieInfo.videos.results.filter((trailer) => trailer.type === 'Trailer').at(0).key}`" frameborder="0"></iframe>
     </div>
 
-    <div v-if="movie-info" class="grid-container detailInfo"> 
+    <div v-if="movieInfo" class="grid-container detail-info"> 
       <h3>{{ movieInfo.origin_country }}</h3> 
       <h3>{{ movieInfo.release_date }}</h3> 
       <h3>{{ movieInfo.original_title }}</h3> 
@@ -40,11 +40,135 @@
       <p>Runtime:{{ movieInfo.runtime }}</p> 
       <p>Status:{{ movieInfo.status }}</p> 
       <p>Rating:{{ movieInfo.vote_average }}</p> 
-
     </div>
-    </div>
-
+  </div>
 </template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  background-image: url('https://wall.alphacoders.com/big.php?i=749455');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+
+header h1 {
+  border-radius: 15px;
+  background-color: #4d4c7d;
+  text-shadow: 5px 5px #827397;
+  filter: drop-shadow(-10px 10px 20px #827397);
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  padding: 50px;
+  font-weight: bold;
+  line-height: 1 rem;
+  text-align: left;
+  color: #d8b9c3;
+  text-align: left;
+  padding: 2rem;
+  margin: 0;
+  font-size: 2rem;
+  letter-spacing: 1px;
+}
+
+label {
+  color: #d8b9c3;
+  margin-right: 10px;
+  font-size: 2rem;
+  font-weight: bold;
+}
+
+.movie-picker {
+ 
+display: flex;
+align-items: center;
+justify-content: center;
+margin-bottom: 20px;
+padding: 8px;
+font-size: 1rem;
+border-radius: 5px;
+}
+
+button {
+margin-left: auto;
+padding: 8px 15px;
+border: none;
+border-radius: 5px;
+background-color: #d8b9c3;
+color: white;
+font-size: 2rem;
+cursor: pointer;
+filter: drop-shadow(-10px 10px 20px #827397);
+}
+button:hover {
+background-color: #dba4b5;
+color: white;
+filter: drop-shadow(-10px 10px 10px #827397);
+}
+
+.grid-container {
+display: grid;
+grid-gap: 20px;
+margin: 20px;
+background-color: rgba(252, 210, 235, 0.25);
+}
+
+.basic-info {
+font-size: 30px;
+text-align: center;
+color: white;
+padding: 20px;
+border-radius: 5px;
+box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+background-color: rgba(252, 210, 235, 0.25);
+}
+
+.img {
+width: 40px;
+margin-right: 20px;
+margin-bottom: 10px;
+border-radius: 5px;
+}
+
+#movie-trailer {
+width: 50%;
+height: 500px;
+padding: 3rem;
+border: none;
+border-radius: 5px;
+}
+
+.basic-info {
+color: white;
+justify-content: center;
+padding: 20px;
+border-radius: 5px;
+box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+background-color: rgba(252, 210, 235, 0.25);
+}
+
+.movie-details {
+margin-top: 0;
+font-size: 1.5rem;
+}
+
+#movie-picker {
+border-radius: 15px;
+border-color: rgba(0, 0, 0, 0);
+padding: 1rem;
+font-size: 1.5rem;
+background-color: #4d4c7d;
+color: #d8b9c3;
+filter: drop-shadow(-10px 10px 20px #827397);
+font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+option:hover {
+filter: drop-shadow(-10px 10px 10px #827397);
+background-color: black;
+}
+</style>
 
 <script>
 import { ref } from "vue";
@@ -76,142 +200,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-
-* {
-  padding: 0;
-  margin: 0;
-}
-
-body {
-  background-size: 100vw;
-  margin-bottom: 8%;
-  /* background-image: url('./assets/images/handsome.jpg'); */
-  width: 100vw;
-}
-
-#background {
-    position: absolute;
-    z-index: -1;
-    width:100%;
-    height:100%;
-  }
-
-header h1 {
-  border-radius: 15px;
-  background-color: #4d4c7d;
-  text-shadow: 5px 5px #827397;
-  filter: drop-shadow(-10px 10px 20px #827397);
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  padding: 50px;
-  font-weight: bold;
-  line-height: 1 rem;
-  text-align: left;
-  color: #d8b9c3;
-  text-align: left;
-  padding: 2rem;
-  margin: 0;
-  font-size: 2rem;
-  letter-spacing: 1px;
-}
-
-label {
-  color: #d8b9c3;
-  margin-right: 10px;
-  font-size: 2rem;
-  font-weight: bold;
-}
-
-.movie-picker {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 20px;
-  padding: 8px;
-  font-size: 1rem;
-  border-radius: 5px;
-}
-
-button {
-  margin-left: auto;
-  padding: 8px 15px;
-  border: none;
-  border-radius: 5px;
-  background-color: #d8b9c3;
-  color: white;
-  font-size: 2rem;
-  cursor: pointer;
-  filter: drop-shadow(-10px 10px 20px #827397);
-}
-button:hover {
-  background-color: #dba4b5;
-  color: white;
-  filter: drop-shadow(-10px 10px 10px #827397);
-}
-
-.grid-container {
-  display: grid;
-  grid-gap: 20px;
-  margin: 20px;
-  background-color: rgba(252, 210, 235, 0.25);
-
-}
-
-.basic-info {
-  font-size: 30px;
-  text-align: center;
-  color: white;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  background-color: rgba(252, 210, 235, 0.25);
-}
-
-#movie-poster {
-  width: 40%;
-  max-width: 400px;
-  margin-right: 20px;
-  margin-bottom: 10px;
-  border-radius: 5px;
-}
-
-#movie-trailer {
-  width: 50%;
-  height: 500px;
-  padding: 3rem;
-  border: none;
-  border-radius: 5px;
-}
-
-.basic-info {
-  color: white;
-  justify-content: center;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  background-color: rgba(252, 210, 235, 0.25);
-}
-
-.movie-details{
- margin-top: 0;
-font-size: 1.5rem; 
-}
-
-#movie-picker {
-  border-radius: 15px;
-  border-color: rgba(0, 0, 0, 0);
-  padding: 1rem;
-  font-size: 1.5rem;
-  background-color: #4d4c7d;
-  color: #d8b9c3;
-  filter: drop-shadow(-10px 10px 20px #827397);
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-}
-
-option:hover{
-  filter: drop-shadow(-10px 10px 10px #827397);
-  background-color: black;
-}
-
-</style>
